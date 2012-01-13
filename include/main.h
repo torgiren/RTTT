df5a3f6c1610251c6fe4d8b5932f4e40d39a1c91
Marcin TORGiren Fabrykowski <torgiren@gmail.com>
2012-01-13 04:14:22 +0100

    jakies opisy do gry w dokumentacji

--- include/main.h
+++ include/main.h
@@ -1,3 +1,12 @@
 /**
- * @mainpage RTTT - Risky Tic Tac Toe. Gra strategiczna laczaca elementy gry Ryzyko z gra "Kolko i Krzyzyk"
+ * @mainpage RTTT - Risky Tic Tac Toe
+ * \section desctiption Opis gry
+ * Gra strategiczna laczaca elementy gry Ryzyko z gra "Kółko i krzyżyk". Fabuła gry osadzona jest w przestrzeni kosmicznej. Twoj zadaniem, jak generała floty, jest odeprzeć inwazję kosmitów, oraz wyeliminować konkurencyjne frakcje
+ * \section rules Reguly panujace w kosmosie
+ * \subsection zdobywanie_planet Zdobywanie planet
+ * Podstawowym elementem gry są posiadane planety. Aby podbić planete, należy umieścić na niej swoje jednoski.  Wyslane jednostki po dotarciu do celu, walczą z stacjonujacymi tam statkami wroga. Po wygranej bitwie, planeta przechodzi w stan okupacji. Jeśli jest to planeta neutralna, należy ją okupować (posiadac tam conajmniej jedną jednostkę) przez 3 tury.<br/>
+ * Jeśli natomiast jest to planeta przeciwnika trzeba odczekać 3 tury na obalenie tamtejszego rządu i kolejne 3 tury na utworzenie swojego.<br/>
+ * Natomiast, jeśli podczas okupacji wróg najedzie na planetę która była okupowana przez 2 dni, pokona jednostki gracza i sam zacznie ją okupować, musi odczekać tylko 2 tury na obalenie tworzonego tam rządu. Dokładnie tyle ile gracz poświęcił na jego utworzenie.
+ * \subsection zdobywanie_jednostek Zdobywanie jednostek
+ * Na każdej pobitej przez gracza planecie produkowane są statki kosmiczne. Tempo tworzenia statków wynisi jeden na turę i zawsze jest tworzony na koniec tury danego gracza. Tak się po wykonaniu swoich manewrów na każdej planecie tworzona jest jedna nowa jednostka. Tyczy sie to również planet gracza, okupowanych przez wroga.
  */
