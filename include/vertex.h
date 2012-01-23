d3e4b3844692c756611109491213beba805dcad8
shr <cremmy@interia.pl>
2012-01-23 14:17:50 +0100

    Optymalizacya~

--- include/vertex.h
+++ include/vertex.h
@@ -3,6 +3,8 @@
  * @brief Prosty vertex/wektor 3D, zawiera podstawowe operacje
  * @details Funkcje rysujące przystosowane są do ułożenia wertexów przeciwnie do ruchu wskazówek zegara (CCW)
  */
+#include <cmath>
+
 class Vertex
 {
 	public:
@@ -44,15 +46,20 @@ class Vertex
 			return Vertex(x*v, y*v, z*v);
 		}
 
-			Vertex operator/(float v) const
+		Vertex operator/(float v) const
 		{
 			return Vertex(x/v, y/v, z/v);
 		}
 
-	/// @brief Iloczyn wektorowy. Z pewnych powodów pomija z. "Taki ficzer".
+	/// @brief Iloczyn wektorowy. Z pewnych powod�w pomija z. "Taki ficzer".
 		Vertex cross(const Vertex& v) const
 		{
 			return Vertex(/*y*v.z-z*v.y*/0, /*z*v.x-x*v.z*/0, x*v.y-y*v.x);
+		}
+	/// @brief Iloczyn wektorowy.
+		Vertex crossz(const Vertex& v) const
+		{
+			return Vertex(y*v.z-z*v.y, z*v.x-x*v.z, x*v.y-y*v.x);
 		}
 
 	/// @brief Iloczyn skalarny
@@ -60,6 +67,13 @@ class Vertex
 		{
 			return x*v.x+y*v.y+z*v.z;
 		}
+
+
+	/// @brief Długosć wektora
+		float len() const
+			{
+			return sqrt(x*x+y*y+z*z);
+			}
 
 		float x, y, z;
 	};
