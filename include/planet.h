e06009038e35f1511dd144f75a566afecca33f96
Marcin TORGiren Fabrykowski <torgiren@gmail.com>
2012-01-29 01:58:09 +0100

    udokumentowana planeta

--- include/planet.h
+++ include/planet.h
@@ -6,46 +6,45 @@
 #include <string>
 /**
  * @brief Klasa planety
- * @details Opisuje właściwości planety - elementrarnej jednoski przestrzeni
+ * @details Opisuje właściwości planety - elementarnej jednostki przestrzeni
  * @author Marcin TORGiren Fabrykowski
  */
 class Planet
 {
-	friend class GameEngine;
 	public:
 		/**
-		 * @brief Tworzy planete
-		 * @details Konstruktor. Tworzy neutralna planete z losowa (od 0 do 9) liczbą jednostek
+		 * @brief Tworzy planetę
+		 * @details Konstruktor. Tworzy neutralna planetę z losowa (od 0 do 9) liczbą jednostek
 		 */
 		Planet();
 		/**
 		 * @brief Zwraca numer gracza-właściciela planety
-		 * @details Zwraca numer gracza który jest aktualnie posiadaczem planety. Planeta może byc okupowana przez innego gracza i wciąż być w posiadaniu starego właściciela
+		 * @details Zwraca numer gracza który jest aktualnie posiadaczem planety. Planeta może być okupowana przez innego gracza i wciąż być w posiadaniu starego właściciela
 		 * @return Zwraca numer gracza który jest właścicielem planety, bądź NULL jeśli takiego nie ma
 		 */
 		uint16 RetGracz() const;
 		/**
 		 * @brief Zwraca numer gracza-okupanta planety
 		 * @details Zwraca numer gracza który jest aktualnie okupantem planety
-		 * @return Numer gracza który okupuje planete, badź NULL jeśli takowego nie ma
+		 * @return Numer gracza który okupuje planetę, bądź NULL jeśli takowego nie ma
 		 */
 		uint16 RetOkupant() const;
 		/**
 		 * @brief Zwracam poziom zaawansowania okupacji
-		 * @details Zwraca aktualny poziom okupacji. Wartość OCCUPY_MAX oznacza, ze planeta nie jest juz okupowana i jest w pełni przejęta
+		 * @details Zwraca aktualny poziom okupacji. Wartość OCCUPY_MAX oznacza, ze planeta nie jest już okupowana i jest w pełni przejęta
 		 * @return Poziom okupacji, bądź OCCUPY_MAX w przypadku gdy planeta nie jest okupowana i jest w pełni przejęta
 		 */
 		uint16 RetPoziom() const;
 		/**
-		 * @brief Zwraca ilosc jednostek na planecie
-		 * @defails Funkcja wraca liczbę floty znajdującej się na planecie. Jeśli planeta nie jest okupowana, jest to liczba jednostek gracza będącego właścicielem, natomiast w przypadku okupacji, jest to liczba jednostek okupanta
+		 * @brief Zwraca ilość jednostek na planecie
+		 * @details Funkcja wraca liczbę floty znajdującej się na planecie. Jeśli planeta nie jest okupowana, jest to liczba jednostek gracza będącego właścicielem, natomiast w przypadku okupacji, jest to liczba jednostek okupanta
 		 * @return Liczba jednostek właściciela planety. W przypadku gdy planeta jest okupowana, to jest liczba jednostek okupanta
 		 */
 		uint16 RetJednostki() const;
 		/**
-		 * @brief Przeprowadza atak na planete
-		 * @detail Przeprowadza atak zadanej ilości jednostek na planete.
-		 * @param ile Liczba jednostek wroga, biąrąca udział w ataku
+		 * @brief Przeprowadza atak na planetę
+		 * @detail Przeprowadza atak zadanej ilości jednostek na planetę.
+		 * @param ile Liczba jednostek wroga, biorąca udział w ataku
 		 * @param kogo Numer gracza który przeprowadza atak
 		 * @return Zwraca wektor reprezentujący kolejne starcia, zawierający pary wektorów rzutów 
 		 * W przypadku mniejszej ilości jednostek po którejś ze stron, w miejsce rzutu wstawiana jest wartość 0
@@ -63,7 +62,18 @@ class Planet
 		 * W przypadku posiadanych planet, następuje tworzenie nowych jednostek
 		 */
 		RETURNS::ENDTURN EndTurn();
+		/**
+		 * @brief Zabiera z planety zadaną liczbę jednostek
+		 * @details Zmniejsza liczbę jednostek na danej planecie o zadaną zwartość. Sprawdza tylko czy zadana wartość jest mniejsza bądź równa ilości jednostek na planecie
+		 * @param ile Zadana ilość jednostek do zabrania
+		 * @return Zwraca status operacji
+		 */
 		RETURNS::MOVE Zabierz(uint16 ile);
+		/**
+		 * @brief Dodaje jednostki do planety
+		 * @details Zwiększa liczbę jednostek na planecie o zadaną ilość
+		 * @param ile Liczba jednostek które zostaną dodane do garnizonu planety
+		 */
 		void Dodaj(uint16 ile);
 		std::string ToString();
 		operator std::string();
