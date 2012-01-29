fa387cb3e16679edd9706341a78974cc149e9789
Marcin TORGiren Fabrykowski <torgiren@gmail.com>
2012-01-29 02:07:23 +0100

    wlaczanie okna

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,33 @@
 #include <iostream>
 #include <string>
 #include <cstdio>
+#include <SDL/SDL_thread.h>
+#include <SDL/SDL.h>
 #include "gameengine.h"
+#include "gameengineclient.h"
+#include "Participant.hpp"
+#include "Room.hpp"
+#include "Message.hpp"
+#include "Session.hpp"
+#include "Server.hpp"
+#include "Client.hpp"
+#include "sprite.h"
+#include "text.h"
+#include "drawing.h"
+#include "screen.h"
 using namespace std;
+int ServerFunc(void* engine);
 int main(int argc, char* argv[])
 {
+	bool server=false;
+	SDL_Thread* Server_thread=0;
 	cout<<"Witaj poruczniku...\nPlanujesz swoją własną bitwę czy dołączyć się do innej?\n";
 menu:
 	cout<<"1) Własna bitwa\n";
 	cout<<"2) Przyłączenie sie do istniejącej"<<endl;
-	char opt;
-	scanf("%c",&opt);
-	bool server=false;
-	switch(opt)
+	char opt[255];
+	cin>>opt;
+	switch(opt[0])
 	{
 		case '1':
 			server=true;
@@ -24,6 +39,14 @@ menu:
 			cout<<"Jakieś <trzask> zakłócenia <trzask>. Proszę powtórzyć wybór..."<<endl;
 			goto menu;
 	};
+	string ip;
+	if(!WindowEngine::init(WindowEngine::SDL, WindowEngine::DELAY))
+	{
+		printf("Nie udalo sie stworzyc okienka\n");
+		return 100;
+	}
+	Drawing::setSurface(WindowEngine::getScreen());
+	Screen::init();
 	if(server)
 	{
 		int size,graczy;
@@ -32,13 +55,35 @@ menu:
 		cout<<"Na ilu przewidujesz przeciwników?"<<endl;	
 		cin>>graczy;
 		cout<<"Tak jest... Przygotowuję odpowiedni obszar..."<<endl;
-		GameEngine silnik(size,graczy);
+		Server_thread=SDL_CreateThread(ServerFunc,(void*)new GameEngine(size,graczy));
+		Screen::setSize(size);
+		ip="127.0.0.1";
 	}
 	else
 	{
 		cout<<"Proszę wprowadzić cztery współrzędne bitwy (xxx.xxx.xxx.xxx):"<<endl;
-		string ip;
 		cin>>ip;
+		Screen::setSize(3);
 	};
+
+	while(WindowEngine::update())
+	{
+		Screen::update();
+		Screen::draw();
+
+		WindowEngine::print();
+	}
+
+	Sprite::clear();
+	WindowEngine::quit();
+//	SDL_Delay(1000);
+//    Client* c=new Client(*SocketSingleton::get(),ip.c_str(), "2332");
+//	GameEngineClient();
+	if(server)
+		SDL_WaitThread(Server_thread,NULL);
+	return 0;
+};
+int ServerFunc(void* engine)
+{
 	return 0;
 };
